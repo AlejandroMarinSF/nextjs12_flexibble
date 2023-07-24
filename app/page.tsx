fe5bb5be6e3eb32505a9dc8bb3c86bc7b3cbc84d
Alejandro Marin <alejandromarin199901@gmail.com>
2023-07-24 15:25:18 -0300

    Creating the Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className="flex-start flex-col paddings mb-16">
+           <h1>Categories</h1>
+           <h1>posts</h1>
+           <h1>loadmore</h1>
+        </section>
     )
 }
 
