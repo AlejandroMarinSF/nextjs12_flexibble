499ef1b56684c79e9e0a341d1a2aa7579e34614b
Alejandro Marin <alejandromarin199901@gmail.com>
2023-07-26 16:51:37 -0300

    Adding grafbase to the project

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { getProviders, signIn } from 'next-auth/react';
+import { useState, useEffect } from "react";
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider> 
+
+const AuthProviders = () => {
+    const [providers, setProviders] = useState<Providers | null>(null);
+  return (
+    <div>AuthProviders</div>
+  )
+}
+
+export default AuthProviders
